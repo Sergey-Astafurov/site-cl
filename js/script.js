6dbcceb66fc5a96a31d5b0e49d3642d3b5fde663
Sergey Astafurov <stupidsociety16@gmail.com>
2021-12-15 19:58:14 +0600

    all

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// let card = document.querySelectorAll('.card')
+// let cardImg = document.querySelectorAll('.card-img')
+// card.forEach(elem => {
+//     elem.addEventListener('mouseover', function(event){
+//         event = event.target ;
+//         event.style.transform = "translateY(-30px)";
+//         // cardImg.forEach(function (event) {
+//         //     event = event.target ;
+//         //     event.style.filter = 'grayscale(0)';
+        
+                
+//         // })
+//     })
+// });
